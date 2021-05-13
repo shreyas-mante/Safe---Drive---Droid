9c73bacc449794cea3cb5319e63f303adc631a13
Shreyas Mante <shreyas.mante@gmail.com>
2021-05-13 20:55:51 +0530

    PROJECT DONE

---
+++ app/src/main/java/com/gpp/Adapters/PageAdapter.java
@@ -0,0 +1,57 @@
+package com.gpp.Adapters;
+
+/**
+ * Adapter class for implementing view pager
+ */
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.gpp.Fragments.AutoReplyFragment;
+import com.gpp.Fragments.CallLogsFragment;
+import com.gpp.Fragments.DriveModeFragment;
+import com.drigio.labs.callblock.R;
+
+public class PageAdapter extends FragmentStatePagerAdapter {
+
+    //Declare your class variables here
+    int noOfTabs;
+    String[] tabTitles;
+
+    public PageAdapter(FragmentManager fm, int noOfTabs, Context context) {
+        super(fm);
+        this.noOfTabs = noOfTabs;
+        //Set the titles of the tabs on the LaunchActivity
+        tabTitles = new String[]{
+                context.getString(R.string.drive_mode_tab),
+                context.getString(R.string.auto_reply_tab),
+                context.getString(R.string.call_logs_tab)
+        };
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0 :
+                return new DriveModeFragment();
+            case 1 :
+                return new AutoReplyFragment();
+            case 2 :
+                return new CallLogsFragment();
+            default :
+                    return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return noOfTabs;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabTitles[position];
+    }
+}
